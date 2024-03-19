5e2bbe268a6f6510e283ae5babf894e636e8a066
alishasinha0016 <59633261+alishasinha0016@users.noreply.github.com>
2024-03-19 14:39:24 -0700

    Update LCA.java

--- HW/HW9/LCA.java
+++ HW/HW9/LCA.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -14,6 +16,12 @@ class TreeNode {
 public class LCA {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.equals(q)|| (!isValidNode(root, p)) || (!isValidNode(root, q))||
+        (countNumberOfNodes(root) >= 2 && countNumberOfNodes(root) <= 100000) ||
+        (!NodeValueChcek(root)) || (!areAllValuesUnique(root)) ){
+    throw new IllegalArgumentException("The conditions are not met");
+}
+
         if (root == null || root == p || root == q) {
             return root;
         }
@@ -31,6 +39,62 @@ public class LCA {
             return right;
         }
     }
+    public int countNumberOfNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+    
+        int leftCount = countNumberOfNodes(root.left);
+        int rightCount = countNumberOfNodes(root.right);
+    
+        return 1 + leftCount + rightCount;
+    }
+    public boolean NodeValueChcek(TreeNode node){
+            if (node == null) {
+                return true;
+            }
+        
+            if (node.val < -1000000000 || node.val > 1000000000) {
+                return false;
+            }
+        
+            return NodeValueChcek(node.left) && NodeValueChcek(node.right);
+        }
+        
+    public boolean areAllValuesUnique(TreeNode root) {
+    Set<Integer> seenValues = new HashSet<>();
+    return checkUniqueness(root, seenValues);
+}
+
+private boolean checkUniqueness(TreeNode node, Set<Integer> seenValues) {
+    if (node == null) {
+        return true;
+    }
+
+    if (!seenValues.add(node.val)) {
+        return false;
+    }
+
+    return checkUniqueness(node.left, seenValues) && checkUniqueness(node.right, seenValues);
+}
+
+public boolean isValidNode(TreeNode root, TreeNode node) {
+    if (root == null) {
+        return false;
+    }
+
+    if (root == node) {
+        return true;
+    }
+
+    if (node.val < root.val) {
+        return isValidNode(root.left, node);
+    } else {
+        return isValidNode(root.right, node);
+    }
+    
+}
+    
 
     public static void main(String[] args) {
         LCA lca = new LCA();
@@ -49,4 +113,3 @@ public class LCA {
         System.out.println("Lowest Common Ancestor is: " + ancestor.val); // Output: 2
     }
 }
-
