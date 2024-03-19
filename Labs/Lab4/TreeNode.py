d65fa2d9efef8f84970f61eea9309c3a89d874e3
alishasinha0016 <59633261+alishasinha0016@users.noreply.github.com>
2024-03-19 16:34:17 -0700

    Update TreeNode.py

--- Labs/Lab4/TreeNode.py
+++ Labs/Lab4/TreeNode.py
@@ -1,9 +1,53 @@
-class TreeNode(object):
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-     def __init__(self, val=0, left=None, right=None):
+def invertTree(root):
+    if root is None:
+        return None
 
-         self.val = val
+    # Here we are using recursion to make the swap happen till we reach the lowest subtree
+    left = invertTree(root.left)
+    right = invertTree(root.right)
+    # Here we are swapping the left and right subtree
+    root.left = right
+    root.right = left
 
-         self.left = left
+    return root
 
-         self.right = right
+def printTreeLevelByLevel(root):
+    if root is None:
+        return
+
+    currentTreeLevel = [root]
+
+    while currentTreeLevel:
+        nextTreeLevel = []
+        for node in currentTreeLevel:
+            print(node.val, end=" ")
+
+            if node.left:
+                nextTreeLevel.append(node.left)
+            if node.right:
+                nextTreeLevel.append(node.right)
+
+        print()
+        currentTreeLevel = nextTreeLevel
+
+def main():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(6)
+
+    invertedRoot = invertTree(root)
+    printTreeLevelByLevel(invertedRoot)
+
+if __name__ == "__main__":
+    main()
+
