2049a5d6f1e1efec8bce0054bf8ba0b28d996ca1
alishasinha0016 <59633261+alishasinha0016@users.noreply.github.com>
2024-03-19 13:22:42 -0700

    Update LCA.java

--- HW/HW9/LCA.java
+++ HW/HW9/LCA.java
@@ -1,1 +1,52 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class LCA {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        if(left != null){
+            return left;
+        }
+        else{
+            return right;
+        }
+    }
+
+    public static void main(String[] args) {
+        LCA lca = new LCA();
+
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(1);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(9);
+
+        TreeNode p = root.left; 
+        TreeNode q = root.left.left; 
+
+        TreeNode ancestor = lca.lowestCommonAncestor(root, p, q);
+        System.out.println("Lowest Common Ancestor is: " + ancestor.val); // Output: 2
+    }
+}
 
