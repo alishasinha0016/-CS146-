24118ff4724d733955dff514cdbb7ef731fbab10
alishasinha0016 <59633261+alishasinha0016@users.noreply.github.com>
2024-03-19 18:39:09 -0700

    Update TreeNode.java

--- Labs/Lab5/TreeNode.java
+++ Labs/Lab5/TreeNode.java
@@ -1,24 +1,68 @@
- public class TreeNode {
+import java.util.*;
 
-     int val;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-     TreeNode left;
+    TreeNode() {
+    }
 
-     TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+    }
 
-     TreeNode() {}
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
 
-     TreeNode(int val) { this.val = val; }
+    }
 
-     TreeNode(int val, TreeNode left, TreeNode right) {
+}
+class validateBST {
 
-         this.val = val;
+    public static void main(String[] args) {
+        // Constructing 1st BST
+        TreeNode input_1_BST = new TreeNode(4);
+        input_1_BST.left = new TreeNode(3);
+        input_1_BST.left.left = new TreeNode(1);
+        input_1_BST.right = new TreeNode(8);
+        input_1_BST.right.left = new TreeNode(5);
+        input_1_BST.right.right = new TreeNode(9);
 
-         this.left = left;
+        // Constructing 2nd BST
+        TreeNode input_2_BST = new TreeNode(1);
+        input_2_BST.left = new TreeNode(2);
+        input_2_BST.right = new TreeNode(8);
+        input_2_BST.left.left = new TreeNode(3);
+        input_2_BST.left.right = new TreeNode(4);
+        input_2_BST.right.left = new TreeNode(5);
+        input_2_BST.right.right = new TreeNode(6);
 
-         this.right = right;
+        // Testing isValidBST function
+        validateBST ob_BST = new validateBST();
+        System.out.println("Valid BST: " + ob_BST.isValidBST(input_1_BST)); 
+        System.out.println("Invalid BST: " + ob_BST.isValidBST(input_2_BST)); 
+    }
 
-     }
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> inorderListTraversal = new ArrayList<>();
+        inorderTraversal(root, inorderListTraversal);
+        for (int i = 1; i < inorderListTraversal.size(); i++) {
+            if (inorderListTraversal.get(i) <= inorderListTraversal.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-  }
-
+    public void inorderTraversal(TreeNode root, List<Integer> inorderList) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, inorderList);
+        inorderList.add(root.val);
+        inorderTraversal(root.right, inorderList);
+    }
+}
