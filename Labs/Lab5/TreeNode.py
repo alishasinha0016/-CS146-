da782fd92fc21ff8cbbfc5c0d770bdf9b97f6a17
alishasinha0016 <59633261+alishasinha0016@users.noreply.github.com>
2024-03-19 18:52:14 -0700

    Update TreeNode.py

--- Labs/Lab5/TreeNode.py
+++ Labs/Lab5/TreeNode.py
@@ -1,9 +1,48 @@
-class TreeNode(object):
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-     def __init__(self, val=0, left=None, right=None):
+class ValidateBST:
+    def main(self):
+        # Constructing 1st BST
+        input_1_BST = TreeNode(4)
+        input_1_BST.left = TreeNode(3)
+        input_1_BST.left.left = TreeNode(1)
+        input_1_BST.right = TreeNode(8)
+        input_1_BST.right.left = TreeNode(5)
+        input_1_BST.right.right = TreeNode(9)
 
-         self.val = val
+        # Constructing 2nd BST
+        input_2_BST = TreeNode(1)
+        input_2_BST.left = TreeNode(2)
+        input_2_BST.right = TreeNode(8)
+        input_2_BST.left.left = TreeNode(3)
+        input_2_BST.left.right = TreeNode(4)
+        input_2_BST.right.left = TreeNode(5)
+        input_2_BST.right.right = TreeNode(6)
 
-         self.left = left
+        # Testing isValidBST function
+        ob_BST = ValidateBST()
+        print("Valid BST:", ob_BST.isValidBST(input_1_BST))
+        print("Invalid BST:", ob_BST.isValidBST(input_2_BST))
 
-         self.right = right
+    def isValidBST(self, root):
+        inorder_list = []
+        self.inorder_traversal(root, inorder_list)
+        for i in range(1, len(inorder_list)):
+            if inorder_list[i] <= inorder_list[i - 1]:
+                return False
+        return True
+
+    def inorder_traversal(self, root, inorder_list):
+        if root is None:
+            return
+        self.inorder_traversal(root.left, inorder_list)
+        inorder_list.append(root.val)
+        self.inorder_traversal(root.right, inorder_list)
+
+# Running the main function
+if __name__ == "__main__":
+    ValidateBST().main()
