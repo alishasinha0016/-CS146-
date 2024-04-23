68e8e3cd879b4a5132a39167b32ec909fe7e1964
alishasinha0016 <59633261+alishasinha0016@users.noreply.github.com>
2024-04-22 23:26:19 -0700

    Update MST.java

--- HW/HW 12/MST.java
+++ HW/HW 12/MST.java
@@ -1,1 +1,26 @@
-
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+ 
+class MST {
+ 
+    // Number of vertices in the graph
+    private static final int V = 5;
+ 
+    // A utility function to find the vertex with minimum
+    // key value, from the set of vertices not yet included
+    // in MST
+    int minKey(int key[], Boolean mstSet[])
+    {
+        // Initialize min value
+        int min = Integer.MAX_VALUE, min_index = -1;
+ 
+        for (int v = 0; v < V; v++)
+            if (mstSet[v] == false && key[v] < min) {
+                min = key[v];
+                min_index = v;
+            }
+ 
+        return min_index;
+    }
+}
